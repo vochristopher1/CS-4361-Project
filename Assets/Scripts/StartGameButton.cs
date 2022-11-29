31c81731dc30de73600cdef2df0723d998d3b071
Will Pearl <will.pearl200@gmail.com>
2022-11-29 15:50:33 -0600

    Added Main Menu

---
+++ Assets/Scripts/StartGameButton.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartGameButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("asdf");
+    }
+
+    public void StartGame()
+    {
+        Debug.Log("Click!!!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
